e64a540acaff9c0bdb024d6c53a8b10ef4b78a6e
defu3e <defu3e@yahoo.com>
2023-04-09 22:03:41 +0300

    add calling GetBillingData func

--- stageSystem/main.go
+++ stageSystem/main.go
@@ -1,12 +1,31 @@
 package main
 
 import (
+	"fmt"
+	"stageSystem/config"
 	"stageSystem/pkg/billing"
+	"stageSystem/pkg/countrycodes"
 	"stageSystem/pkg/email"
 	"stageSystem/pkg/mms"
 	"stageSystem/pkg/smsgetter"
 	"stageSystem/pkg/voice"
 )
+
+var (
+	smsFile,
+	voiceFile,
+	emailFile,
+	billingFile string
+)
+
+func init () {
+	countrycodes.Init()
+	
+	smsFile = config.GoDotEnvVariable("SMS_FILE_PATH") 
+	voiceFile = config.GoDotEnvVariable("VOICE_FILE_PATH")
+	emailFile = config.GoDotEnvVariable("EMAIL_FILE_PATH")
+	billingFile = config.GoDotEnvVariable("BILLING_FILE_PATH")
+}
 
 func main () {
 	smsgetter.GetSmsData(smsFile)
@@ -17,5 +36,5 @@ func main () {
 
 	email.GetEmailData(emailFile)
 
-	billing.GetBillingData(billingFile)
+	fmt.Println (billing.GetBillingData(billingFile))
 }
