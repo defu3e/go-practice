ab02ea8aec67edf657282482df3191c1d4a9bd2c
defu3e <defu3e@yahoo.com>
2023-04-15 18:28:36 +0300

    move result strcuturies and methods to own pkg

---
+++ stageSystem/pkg/result/result.go
@@ -0,0 +1,58 @@
+package result
+
+import (
+	"stageSystem/pkg/billing"
+	"stageSystem/pkg/email"
+	"stageSystem/pkg/incident"
+	"stageSystem/pkg/mms"
+	"stageSystem/pkg/sms"
+	"stageSystem/pkg/support"
+	"stageSystem/pkg/voice"
+)
+
+type ResultSetT struct {
+	SMS       [][]sms.SMSData					`json:"sms"`
+	MMS       [][]mms.MMSData					`json:"mms"`
+	VoiceCall []voice.VoiceData					`json:"voice_call"`
+	Email     map[string][][]email.EmailData	`json:"email"`
+	Billing   billing.BillingData				`json:"billing"`
+	Support   []int								`json:"support"`
+	Incidents []incident.IncidentData			`json:"incident"`
+}
+
+type ResultT struct {
+	Status bool       `json:"status"` 
+	Data   ResultSetT `json:"data"`   
+	Error  string     `json:"error"` 
+}
+
+func GetResultData() (res ResultSetT) {
+	smsData := sms.GetSmsData()
+	res.SMS = sms.PrepareSmsData(smsData)
+
+	mmsData := mms.GetMmsStatus()
+	res.MMS = mms.PrepareMmsData(mmsData)
+	
+	res.VoiceCall = voice.GetVoiceData()
+
+	e := email.GetEmailData()
+	res.Email = email.PrepareEmailData(e) 
+
+	res.Billing = billing.GetBillingData()
+
+	sup := support.GetSupportData()
+	res.Support = support.PrepareSupportData(sup)
+
+	incd := incident.GetIncedents()
+	res.Incidents = incident.PrepareIncidentsData(incd)
+	
+	return
+}
+
+func PrepareResponce (data ResultSetT) ResultT {
+	return ResultT{
+		Status: true,
+		Data: data,
+		Error: "",
+	}
+}
